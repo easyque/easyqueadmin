6e75a9767ed22b99919bdf81bc6962276a5469c6
easyque <106196009+easyque@users.noreply.github.com>
2022-05-27 17:17:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = ()=>{
   return(
     <div>
-      <h1>Welcome into easyque dashboard...</h1>
+      <h1>Welcome into easyque dashboard sdfdsfdf...</h1>
     </div>
   )
 }
